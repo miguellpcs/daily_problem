25bbd0eb320a927de41d2c9a6fd1b790bf8115e2
Miguel Cruz <mlpcs@cin.ufpe.br>
2020-10-09 21:01:32 -0300

    Day 2

--- day1.py
+++ day1.py
@@ -1,12 +1,12 @@
-def solution(arr,val):
-    if len(arr) < 2:
+def solution(arr,val): #O(n)
+    if len(arr) < 2: # O(n)
         return False
 
     previous_numbers = {}
-    
-    for cur in arr:
-        if (val - cur) in previous_numbers.keys():
-            return True
-        previous_numbers[cur] = True
+
+    for cur in arr: # O(n)
+        if (val - cur) in previous_numbers.keys(): # O(1)
+            return True 
+        previous_numbers[cur] = True # O(n)
     return False
 
