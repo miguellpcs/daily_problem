ca0a713579e045700fa369c7daf121d52201e85e
Miguel Cruz <mlpcs@cin.ufpe.br>
2020-10-09 21:13:50 -0300

    Day 1!

--- day1.py
+++ day1.py
@@ -1,4 +1,17 @@
 def solution(arr,val): #O(n)
+    """
+    Description:  Given a list of numbers and a number k, 
+    return whether any two numbers from the list add up to k.
+
+    Input: List of numbers arr, Integer val
+    Output: True or False
+
+    This algorithm has linear complexity since for each number on the list it will verify if the number that 
+    adds up to Val is a valid key on the dictionary of seen numbers. The second task takes constant time since
+    the "in" operator is implement with hash tables to dictionaries. 
+
+
+    """
     if len(arr) < 2: # O(n)
         return False
 
